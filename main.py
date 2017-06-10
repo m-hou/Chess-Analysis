fd732f9bdabc9f641c7a159f9c39f9fb4574a897
m-hou <michaeldamush@gmail.com>
2017-06-10 15:46:39 -0700

    connect fen sequence to game node

--- main.py
+++ main.py
@@ -12,30 +12,36 @@ def parse_first_game():
     first_game = chess.pgn.read_game(pgn)
     pgn.close()
 
-    node = first_game
+    game = first_game
     fens = []
     moves = []
-    while not node.is_end():
-        next_node = node.variation(0)
-        fens.append(node.board().board_fen() + (" b", " w")[node.board().turn])
-        moves.append(node.board().san(next_node.move))
-        node = next_node
-    fens.append(node.board().board_fen() + (" b", " w")[node.board().turn])
-    return fens, moves
+    while not game.is_end():
+        next_node = game.variation(0)
+        fens.append(game.board().board_fen() + (" b", " w")[game.board().turn])
+        moves.append(game.board().san(next_node.move))
+        game = next_node
+    fens.append(game.board().board_fen() + (" b", " w")[game.board().turn])
+    return fens, moves, first_game
 
 def neo4j():
     """doc"""
     driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "neo4j"))
     session = driver.session()
-    fens, moves = parse_first_game()
+    fens, moves, game = parse_first_game()
     print(len(fens), len(moves))
     print(moves)
+    session.run("MATCH (n) DETACH DELETE n")
     for i, _ in enumerate(moves):
         print(i)
         session.run(
             "MERGE (curr:Position {fen: {currFen}})"
-            "MERGE (curr) -[:Move {move: {move}}]-> (:Position {fen: {nextFen}})",
-            {"currFen": fens[i], "nextFen": fens[i+1], "move": moves[i]}
+            "MERGE (curr) -[:Move {move: {move}}]-> (next:Position {fen: {nextFen}})"
+            "MERGE (game:Game {elo: {elo}, timeControl: {timeControl}, result: {result}})"
+            "MERGE (curr) -[:PlayedIn]-> (game)"
+            "MERGE (next) -[:PlayedIn]-> (game)",
+            {"currFen": fens[i], "nextFen": fens[i+1], "move": moves[i],
+             "elo": (int(game.headers["WhiteElo"]) + int(game.headers["BlackElo"])) / 2,
+             "timeControl": game.headers["TimeControl"], "result": game.headers["Result"]}
         )
     session.close()
 
