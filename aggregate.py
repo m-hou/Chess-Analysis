32578e9932906d44b398de1cbce1560f5f5a3899
m-hou <michaeldamush@gmail.com>
2017-06-13 22:30:17 -0700

    add win rate query for memory efficient implementation

--- aggregate.py
+++ aggregate.py
@@ -26,9 +26,36 @@ def get_best_moves():
     print(counter)
     session.close()
 
+def get_best_moves_mem_eff():
+    """doc"""
+    driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "pass"))
+    session = driver.session()
+    result = session.run(
+        """
+        MATCH (g:Game)
+        MATCH (:Position {fen: {fen}})-[m:Move]->(next:Position)
+        WHERE (g)<-[*]-(next)
+        return m.move AS move, SUM(
+            CASE g.result
+                WHEN '1-0' THEN 1
+                WHEN '1/2-1/2' THEN 0.5
+                WHEN '0-1' THEN 0
+            END
+         ) / count(g) AS winrate, count(g) AS freq
+         ORDER BY winrate DESC""",
+        {"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"}
+    )
+    counter = 0
+    for record in result:
+        counter += 1
+        print("%s %s %s" % (record["move"], record["winrate"], record["freq"]))
+    print(counter)
+    session.close()
+
 def main():
     """doc"""
-    get_best_moves()
+    #get_best_moves()
+    get_best_moves_mem_eff()
 
 if __name__ == "__main__":
     main()
