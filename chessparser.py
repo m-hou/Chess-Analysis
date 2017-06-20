2f9262a8a3035636975523259224eba0b4d43a13
m-hou <michaeldamush@gmail.com>
2017-06-19 22:49:52 -0700

    implement pgn library

--- chessparser.py
+++ chessparser.py
@@ -1,47 +1,37 @@
 """doc"""
 
-import chess.pgn
 import sqlite3
 from sqlite3 import IntegrityError
+import pgn
 
 PGN_FILE = "ficsgamesdb_201701_standard2000_nomovetimes_1465638.pgn"
 DB_PATH = "chess.sqlite"
-
-def fetch_game_data(game):
-    eco_code = game.headers["ECO"]
-    time, increment = game.headers["TimeControl"].split("+")
-    result = game.headers["Result"]
-    year, month, _ = game.headers["Date"].split(".")
-    white_elo = game.headers["WhiteElo"]
-    black_elo = game.headers["BlackElo"]
-    gameid = "FICS" + game.headers["FICSGamesDBGameNo"]
-    return result, year, month, white_elo, black_elo, eco_code, time, increment, gameid
 
 def add_games_to_db(inputfile, outputfile):
     """doc"""
     conn = sqlite3.connect(outputfile)
     c = conn.cursor()
-    with open(inputfile) as pgn:
-        curr_game = chess.pgn.read_game(pgn)
-        counter = 0
-        while curr_game != None and counter < 100:
-            result, year, month, white_elo, black_elo, eco_code, time, increment, gameid = fetch_game_data(curr_game)
-            try:
-                c.execute("""INSERT INTO Eco(code)
-                            VALUES (?)""", (eco_code,))
-            except IntegrityError:
-                pass
-            try:
-                c.execute("""INSERT INTO TimeControl(time, increment)
-                            VALUES (?,?)""", (time, increment,))
-            except IntegrityError:
-                pass
-            c.execute("""INSERT INTO Games
-                        VALUES (?,?,?,?,?,?,?,?,?)""", (result, year, month, white_elo, black_elo, eco_code, time, increment, gameid,))
-            counter += 1
-            if counter % 10 == 0:
-                print(counter)
-            curr_game = chess.pgn.read_game(pgn)
+    for index, game in enumerate(pgn.GameIterator(inputfile)):
+        e = (game.eco,)
+        time, increment = game.timecontrol.split("+")
+        t = (time, increment,)
+        year, month, _ = game.date.split(".")
+        g = (game.result, year, month, game.whiteelo, game.blackelo, game.eco, time, increment, game.ficsgamesdbgameno,)
+        try:
+            c.execute("""INSERT INTO Eco(code)
+                        VALUES (?)""", e)
+        except IntegrityError:
+            pass
+        try:
+            c.execute("""INSERT INTO TimeControl(time, increment)
+                        VALUES (?,?)""", t)
+        except IntegrityError:
+            pass
+        c.execute("""INSERT INTO Games
+                    VALUES (?,?,?,?,?,?,?,?,?)""", g)
+        counter = index + 1
+        if counter % 100 == 0:
+            print(counter)
     conn.commit()
     conn.close()
 
