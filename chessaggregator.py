c250aacf2771b0f54d0777a5f5d8eb13b02f341f
m-hou <michaeldamush@gmail.com>
2017-07-02 22:53:43 -0700

    add functionality to aggregator

--- chessaggregator.py
+++ chessaggregator.py
@@ -3,18 +3,41 @@
 import sqlite3
 
 DB_PATH = "chess.sqlite"
+MIN_ELO = 0
+MAX_ELO = 3000
+INCREMENT = 25
 
-def query_db(db_path):
+def query_db(query, *args):
     """doc"""
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    c.execute("""""")
+    c.execute(query, *args)
+    print(c.fetchall())
     conn.commit()
     conn.close()
 
+def query_play_rate():
+    """doc"""
+    query_db(
+        """
+        SELECT opening, GROUP_CONCAT(frequency) frequency
+        FROM (
+            WITH RECURSIVE
+                EloRanges(elo) AS (VALUES(?) UNION ALL SELECT elo + ? FROM EloRanges WHERE elo < ?)
+            SELECT EloRanges.elo elo, Eco.openingName opening,
+                SUM(CASE WHEN Games.gameid IS NULL THEN 0 ELSE 1 END) AS frequency
+            FROM Eco, EloRanges
+            LEFT JOIN Games ON Games.code = Eco.code AND
+            Games.blackElo >= EloRanges.elo and Games.blackElo < EloRanges.elo + ?
+            GROUP BY EloRanges.elo, Eco.openingName
+        ) GROUP BY opening
+        ORDER BY SUM(frequency) DESC LIMIT 20
+        """,
+        (MIN_ELO, INCREMENT, MAX_ELO, INCREMENT))
+
 def main():
     """doc"""
-    query_db(DB_PATH)
+    query_play_rate()
 
 if __name__ == "__main__":
     main()
