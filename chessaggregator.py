f6b3319f5f1aefa053fdde26dcd9ae88092aac43
m-hou <michaeldamush@gmail.com>
2017-07-03 22:45:36 -0700

    add function for query win rate

--- chessaggregator.py
+++ chessaggregator.py
@@ -54,9 +54,32 @@ def query_play_rate():
         play_rate_parser,
         (MIN_ELO, INCREMENT, MAX_ELO, INCREMENT))
 
+@tools.timedcall
+def query_win_rate():
+    """doc"""
+    def win_rate_parser(raw_data):
+        """doc"""
+        opening, wins, draws, losses = list(zip(*raw_data))
+        return [dict(key=result,
+                     values=[dict(x=opening, y=count)
+                             for opening, count in stat_iter])
+                for result, stat_iter in zip(["White wins", "Draws", "Black wins"], [zip(opening, wins), zip(opening, draws), zip(opening, losses)])]
+    query_db(
+        """
+        SELECT Eco.openingName,
+        SUM(CASE WHEN Result = "1-0" THEN 1 ELSE 0 END) AS wins,
+        SUM(CASE WHEN RESULT = "1/2-1/2" THEN 1 ELSE 0 END) as ties,
+        SUM(CASE WHEN RESULT = "0-1" THEN 1 ELSE 0 END) as losses
+        FROM Games
+        INNER JOIN Eco ON Games.code = Eco.code
+        GROUP BY Eco.openingName
+        ORDER BY ((wins + ties / 2) * 1.0 / (wins + ties + losses)) DESC LIMIT 20
+        """,
+        win_rate_parser)
+
 def main():
     """doc"""
-    query_play_rate()
+    query_win_rate()
 
 if __name__ == "__main__":
     main()
