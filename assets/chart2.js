3b03c55ef0cc56abcd6e67bd03f971bfa258bc23
m-hou <michaeldamush@gmail.com>
2017-07-04 22:12:11 -0700

    relabel chart2 y axis and change y scale to percentage

--- assets/chart2.js
+++ assets/chart2.js
@@ -6,6 +6,9 @@ d3.json("assets/data_chart2.json", function(data) {
                 .useInteractiveGuideline(true)
                 .showControls(false);
             chart.color(["#EEEEEE", "#777777", "#000000"])
+            chart.yAxis
+                .axisLabel("Outcome (%)")
+                .tickFormat(d3.format(',.0%'));
             var svg = d3.select('#chart2').datum(data);
             svg.transition().duration(0).call(chart);
 
