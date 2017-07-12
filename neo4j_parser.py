05bc249c6ecfb6a6f2963eaf75c935a58f482c0f
m-hou <michaeldamush@gmail.com>
2017-07-11 23:13:16 -0700

    update to use pgn instead of chess pgn library

--- neo4j_parser.py
+++ neo4j_parser.py
@@ -1,64 +1,49 @@
 """doc"""
 
 import sys
-import chess.pgn
+import pgn
 from neo4j.v1 import GraphDatabase, basic_auth
 
-PGN_FILE = "fics_201701_standard2000_fen.pgn"
+NUMBER_OF_COMMENTS = 1
+STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"
+PGN_FILE = "pgnfiles/output.pgn"
 DB_PATH = "bolt://localhost:7687"
 
-def remove_extra_fen_info(fen):
-    """doc"""
-    return " ".join(fen.split(" ")[:-3])
+def parse_fen(comment):
+    return " ".join(comment[2:-2].split(" ")[:-4])
 
-def parse_games(amount):
-    """doc"""
-    with open(PGN_FILE) as pgn:
-        first_game = chess.pgn.read_game(pgn)
-        counter = 0
-        while first_game != None and counter < amount:
-            game = first_game
-            fens = [remove_extra_fen_info(chess.STARTING_FEN)]
-            moves = []
-            while not game.is_end():
-                next_node = game.variation(0)
-                fens.append(remove_extra_fen_info(next_node.comment))
-                moves.append(game.board().san(next_node.move))
-                game = next_node
-            first_game = chess.pgn.read_game(pgn)
-            counter += 1
-            print(counter)
-            yield fens, moves, first_game
+def get_fens(game):
+    return [STARTING_FEN] + [parse_fen(fen) for fen in game.moves[1:-1:NUMBER_OF_COMMENTS + 1]]
+
+def get_moves(game):
+    return game.moves[:-1:NUMBER_OF_COMMENTS + 1]
 
 def insert(amount=sys.maxsize):
     """doc"""
     driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "pass"))
     session = driver.session()
-    for fens, moves, game in parse_games(amount):
-        for i, _ in enumerate(moves):
-            if i == len(moves) - 1:
+    for count, game in enumerate(pgn.GameIterator(PGN_FILE)):
+        gameid = "FICS" + game.ficsgamesdbgameno
+        result = game.result
+        fens = get_fens(game)
+        moves = get_moves(game)
+        print(count)
+        for index, _ in enumerate(moves):
+            args = {"currFen": fens[index], "nextFen": fens[index+1], "move": moves[index],
+                    "result": result, "gameid": gameid}
+            if index == len(moves) - 1:
                 session.run(
                     "MERGE (curr:Position {fen: {currFen}})"
                     "MERGE (next:Position {fen: {nextFen}})"
                     "MERGE (curr) -[:Move {move: {move}}]-> (next)"
-                    "MERGE (game:Game {elo: {elo}, timeControl: {timeControl}, result: {result}, ficsid: {ficsid}})"
-                    "MERGE (next) -[:PlayedIn]-> (game)",
-                    {"currFen": fens[i], "nextFen": fens[i+1], "move": moves[i],
-                     "elo": (int(game.headers["WhiteElo"]) + int(game.headers["BlackElo"])) / 2,
-                     "timeControl": game.headers["TimeControl"], "result": game.headers["Result"],
-                     "ficsid": game.headers["FICSGamesDBGameNo"]}
-                )
+                    "MERGE (game:Game {result: {result}, gameid: {gameid}})"
+                    "MERGE (next) -[:PlayedIn]-> (game)", args)
             else:
                 session.run(
                     "MERGE (curr:Position {fen: {currFen}})"
                     "MERGE (next:Position {fen: {nextFen}})"
                     "MERGE (curr) -[:Move {move: {move}}]-> (next)"
-                    "MERGE (game:Game {elo: {elo}, timeControl: {timeControl}, result: {result}, ficsid: {ficsid}})",
-                    {"currFen": fens[i], "nextFen": fens[i+1], "move": moves[i],
-                     "elo": (int(game.headers["WhiteElo"]) + int(game.headers["BlackElo"])) / 2,
-                     "timeControl": game.headers["TimeControl"], "result": game.headers["Result"],
-                     "ficsid": game.headers["FICSGamesDBGameNo"]}
-                )
+                    "MERGE (game:Game {result: {result}, gameid: {gameid}})", args)
     session.close()
 
 def main():
