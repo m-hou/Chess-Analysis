03d70115e04110f93756d44b7970e0e2a5947e2b
m-hou <michaeldamush@gmail.com>
2017-07-13 09:01:03 -0700

    abstract neo4j db query method

--- neo4j_aggregator.py
+++ neo4j_aggregator.py
@@ -4,11 +4,25 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 DB_PATH = "bolt://localhost:7687"
 
-def get_next_moves():
+def query_db(query, parser, args):
     """doc"""
     driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "pass"))
     session = driver.session()
-    result = session.run(
+    result = session.run(query, args)
+    parser(result)
+    session.close()
+
+def get_next_moves():
+    """doc"""
+    def next_moves_parser(result):
+        """doc"""
+        counter = 0
+        for record in result:
+            counter += 1
+            print("%s %s %s" % (record["move"], record["winrate"], record["freq"]))
+        print(counter)
+
+    query_db(
         """
         MATCH (:Position {fen: {fen}})-[m:Move]->(next:Position)
         MATCH (next)-[:Move*..1000]->()<-[:FinalPosition]-(g:Game) WITH DISTINCT g, m
@@ -19,14 +33,9 @@ def get_next_moves():
                 WHEN '0-1' THEN 0
             END
         ) AS winrate, count(g) AS freq""",
+        next_moves_parser,
         {"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq"}
     )
-    counter = 0
-    for record in result:
-        counter += 1
-        print("%s %s %s" % (record["move"], record["winrate"], record["freq"]))
-    print(counter)
-    session.close()
 
 def main():
     """doc"""
