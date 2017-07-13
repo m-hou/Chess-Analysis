fa64db65434d7c202d70afc135563af136f63f29
m-hou <michaeldamush@gmail.com>
2017-07-12 21:18:21 -0700

    clean up query

--- neo4j_aggregator.py
+++ neo4j_aggregator.py
@@ -4,21 +4,21 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 DB_PATH = "bolt://localhost:7687"
 
-def get_best_moves():
+def get_next_moves():
     """doc"""
     driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "pass"))
     session = driver.session()
     result = session.run(
         """
-        MATCH (:Position {fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"})-[m:Move]->(next:Position)
-        MATCH (next)-[:Move*..1000]->()<-[:FinalPosition]-(g:Game) WITH DISTINCT g AS Games, m AS Moves
-        RETURN Moves.move AS move, SUM(
-            CASE Games.result
+        MATCH (:Position {fen: {fen}})-[m:Move]->(next:Position)
+        MATCH (next)-[:Move*..1000]->()<-[:FinalPosition]-(g:Game) WITH DISTINCT g, m
+        RETURN m.move AS move, SUM(
+            CASE g.result
                 WHEN '1-0' THEN 1
                 WHEN '1/2-1/2' THEN 0.5
                 WHEN '0-1' THEN 0
             END
-        ) / count(Games) AS winrate, count(Games) AS freq""",
+        ) / count(g) AS winrate, count(g) AS freq""",
         {"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq"}
     )
     counter = 0
@@ -30,7 +30,7 @@ def get_best_moves():
 
 def main():
     """doc"""
-    get_best_moves()
+    get_next_moves()
 
 if __name__ == "__main__":
     main()
