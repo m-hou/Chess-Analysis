e00827112e35e83886f06110c5e2aa7c5ac7c3f0
m-hou <michaeldamush@gmail.com>
2017-07-13 22:46:13 -0700

    added chart 3 to html

---
+++ assets/chart3.js
@@ -0,0 +1,18 @@
+d3.json("assets/data_chart3.json", function(data) {
+    var chart;
+
+    nv.addGraph(function() {
+      var chart = nv.models.boxPlotChart()
+          .x(function(d) { return d.label })
+          .staggerLabels(true)
+          .maxBoxWidth(75); // prevent boxes from being incredibly wide
+
+      d3.select('#chart3')
+          .datum(data)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+})
