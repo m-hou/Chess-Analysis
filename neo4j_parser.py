dc0f855f0f8d9db1098bab872a6e520a5ea25ba2
m-hou <michaeldamush@gmail.com>
2017-07-15 21:15:08 -0700

    include timecontrol property in parser

--- neo4j_parser.py
+++ neo4j_parser.py
@@ -32,19 +32,26 @@ def parse_move_comments(game):
     partitions = NUMBER_OF_COMMENTS + 1
     return [transform(game.moves[i:-1:partitions], i) for i in range(partitions)]
 
+def parse_timecontrol(game):
+    """doc"""
+    for timecontrol in ["standard", "blitz", "lightning"]:
+        if timecontrol in game.event:
+            return timecontrol.capitalize()
 
 def insert(amount=sys.maxsize):
     """doc"""
     driver = GraphDatabase.driver(DB_PATH, auth=basic_auth("neo4j", "pass"))
     session = driver.session()
     for count, game in enumerate(pgn.GameIterator(PGN_FILE)):
+        timecontrol = parse_timecontrol(game)
         gameid = "FICS" + game.ficsgamesdbgameno
         result = game.result
         moves, evals, fens = parse_move_comments(game)
         for index, _ in enumerate(moves):
             args = {"currFen": fens[index], "currEval": evals[index],
                     "nextFen": fens[index+1], "nextEval" :evals[index+1],
-                    "move": moves[index], "result": result, "gameid": gameid,
+                    "move": moves[index], "result": result,
+                    "gameid": gameid, "timecontrol": timecontrol,
                     "blackelo": game.blackelo, "whiteelo": game.whiteelo, "ply": index + 1}
             if index == len(moves) - 1:
                 session.run(
@@ -52,7 +59,8 @@ def insert(amount=sys.maxsize):
                     MERGE (curr:Position {fen: {currFen}, eval: {currEval}})
                     MERGE (next:Position {fen: {nextFen}, eval: {nextEval}})
                     MERGE (curr) -[:Move {move: {move}}]-> (next)
-                    MERGE (game:Game {result: {result}, gameid: {gameid}, whiteElo: {whiteelo}, blackElo: {blackelo}})
+                    MERGE (game:Game {result: {result}, gameid: {gameid},
+                           whiteElo: {whiteelo}, blackElo: {blackelo}, timecontrol: {timecontrol}})
                     MERGE (game) -[:FinalPosition]-> (next)
                     MERGE (ply:Ply {moveNumber: {ply}})
                     MERGE (ply) -[:FinalPosition]-> (curr)""", args)
@@ -62,7 +70,8 @@ def insert(amount=sys.maxsize):
                     MERGE (curr:Position {fen: {currFen}, eval: {currEval}})
                     MERGE (next:Position {fen: {nextFen}, eval: {nextEval}})
                     MERGE (curr) -[:Move {move: {move}}]-> (next)
-                    MERGE (game:Game {result: {result}, gameid: {gameid}, whiteElo: {whiteelo}, blackElo: {blackelo}})
+                    MERGE (game:Game {result: {result}, gameid: {gameid},
+                           whiteElo: {whiteelo}, blackElo: {blackelo}, timecontrol: {timecontrol}})
                     MERGE (game) -[:HasPosition]-> (next)
                     MERGE (ply:Ply {moveNumber: {ply}})
                     MERGE (ply) -[:HasPosition]-> (curr)""", args)
@@ -73,7 +82,7 @@ def insert(amount=sys.maxsize):
 
 def main():
     """doc"""
-    insert()
+    insert(100)
 
 if __name__ == "__main__":
     main()
