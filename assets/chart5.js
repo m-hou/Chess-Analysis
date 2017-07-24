4204b92d59704525024c0fca58a9c065a8e39557
m-hou <michaeldamush@gmail.com>
2017-07-23 21:57:20 -0700

    add tooltip formatting for chart 5

--- assets/chart5.js
+++ assets/chart5.js
@@ -8,9 +8,36 @@ d3.json("assets/data_chart5.json", function (data) {
             .showDistY(true)
             .useVoronoi(true)
             .color(d3.scale.category10().range())
-            .pointRange([100,100])
+            .pointRange([100, 100])
             .duration(300);
+        chart.tooltip.contentGenerator(function (e) {
+            var series = e.series[0];
+            if (series.value === null) return;
+            var rows =
+                "<tr>" +
+                "<td class='key'>" + 'Win Rate: ' + "</td>" +
+                "<td class='x-value'>" + (series.value ? (series.value * 100).toFixed(2) + "%" : 0) + "</td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td class='key'>" + 'Play Rate: ' + "</td>" +
+                "<td class='x-value'>" + (e.value ? (e.value * 100).toFixed(2) + "%" : 0) + "</td>" +
+                "</tr>";
 
+            var header =
+                "<thead>" +
+                "<tr>" +
+                "<td class='legend-color-guide'><div style='background-color: " + series.color + ";'></div></td>" +
+                "<td class='key'><strong>" + e.point.move + "</strong></td>" +
+                "</tr>" +
+                "</thead>";
+
+            return "<table>" +
+                header +
+                "<tbody>" +
+                rows +
+                "</tbody>" +
+                "</table>";
+        });
         chart.xAxis
             .axisLabel('Play Rate (%)')
             .tickFormat(d3.format(',.02%'));
