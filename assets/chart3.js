f0cd33ed95ed8b18441602602cc067cd8dba7221
m-hou <michaeldamush@gmail.com>
2017-07-23 23:38:54 -0700

    add value formatter for chart 3

--- assets/chart3.js
+++ assets/chart3.js
@@ -6,14 +6,16 @@ d3.json("assets/data_chart3.json", function (data) {
             .x(function (d) { return d.label })
             .y(function (d) { return d.value })
             .maxBoxWidth(75); // prevent boxes from being incredibly wide
+        chart.tooltip.valueFormatter(function (d) {
+            return d.toFixed(2);
+        })
+
         chart.xAxis.axisLabel("Time Control")
         chart.yAxis.axisLabel('Evaluation Range in a Game')
 
         d3.select('#chart3')
             .datum(data)
             .call(chart);
-
-
 
         nv.utils.windowResize(chart.update);
 
