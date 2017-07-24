aa75a6e4bf8dd08a036bf0e974ca255d255c422a
m-hou <michaeldamush@gmail.com>
2017-07-23 17:40:07 -0700

    remove extraneous function in chart

--- assets/chart5.js
+++ assets/chart5.js
@@ -1,13 +1,3 @@
-// register our custom symbols to nvd3
-// make sure your path is valid given any size because size scales if the chart scales.
-nv.utils.symbolMap.set('thin-x', function (size) {
-    size = Math.sqrt(size);
-    return 'M' + (-size / 2) + ',' + (-size / 2) +
-        'l' + size + ',' + size +
-        'm0,' + -(size) +
-        'l' + (-size) + ',' + size;
-});
-
 d3.json("assets/data_chart5.json", function (data) {
     // create the chart
     var chart;
