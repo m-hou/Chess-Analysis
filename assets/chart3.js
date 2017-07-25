0095d1da5c77e16de2461747e0157c0c0e244c7b
m-hou <michaeldamush@gmail.com>
2017-07-24 23:28:49 -0700

    update formatter to support whisker value being in key

--- assets/chart3.js
+++ assets/chart3.js
@@ -6,8 +6,51 @@ d3.json("assets/data_chart3.json", function (data) {
             .x(function (d) { return d.label })
             .y(function (d) { return d.value })
             .maxBoxWidth(75); // prevent boxes from being incredibly wide
-        chart.tooltip.valueFormatter(function (d) {
-            return d.toFixed(2);
+        chart.tooltip.contentGenerator(function (d) {
+            var series = d.series;
+            console.log(series[0])
+            if (series.length == 1) {
+                var header =
+                    "<thead>" +
+                    "<tr>" +
+                    "<td class='legend-color-guide'><div style='background-color: " + series[0].color + ";'></div></td>" +
+                    "<td class='key'><strong>" + (series[0].key ? series[0].key.toFixed(2) : 0) + "</strong></td>" +
+                    "</tr>" +
+                    "</thead>";
+
+                return "<table>" +
+                    header +
+                    "</table>";
+            } else {
+                var rows =
+                    "<tr>" +
+                    "<td class='key'>" + series[0].key + ": " + "</td>" +
+                    "<td class='x-value'><strong>" + (series[0].value ? series[0].value.toFixed(2) : 0) + "<strong></td>" +
+                    "</tr>" +
+                    "<tr>" +
+                    "<td class='key'>" + series[1].key + ": " + "</td>" +
+                    "<td class='x-value'><strong>" + (series[1].value ? series[1].value.toFixed(2) : 0) + "<strong></td>" +
+                    "</tr>" +
+                    "<tr>" +
+                    "<td class='key'>" + series[2].key + ": " + "</td>" +
+                    "<td class='x-value'><strong>" + (series[2].value ? series[2].value.toFixed(2) : 0) + "<strong></td>" +
+                    "</tr>";
+
+                var header =
+                    "<thead>" +
+                    "<tr>" +
+                    "<td class='legend-color-guide'><div style='background-color: " + series[0].color + ";'></div></td>" +
+                    "<td class='key'>" + d.key + "</td>" +
+                    "</tr>" +
+                    "</thead>";
+
+                return "<table>" +
+                    header +
+                    "<tbody>" +
+                    rows +
+                    "</tbody>" +
+                    "</table>";
+            }
         })
 
         chart.xAxis.axisLabel("Time Control")
