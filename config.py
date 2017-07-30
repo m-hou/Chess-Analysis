5a052d4570db192238fb6be33ce77c9ab541e57e
m-hou <michaeldamush@gmail.com>
2017-07-30 14:23:51 -0700

    add comment to pgn path in config

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ INCREMENT = 25
 ### PATHS ###
 
 # SOURCES
-SQL_PGN_FILE = "pgnfiles/ficsgamesdb_2016_chess_nomovetimes_1482742.pgn" # 12309953 games
+SQL_PGN_FILE = "pgnfiles/ficsgamesdb_2016_chess_nomovetimes_1482742.pgn" # 12309953 games 2016
 SQL_DB_PATH = "chess.sqlite"
-NEO4J_PGN_FILE = "pgnfiles/output.pgn"
+NEO4J_PGN_FILE = "pgnfiles/output.pgn" # 34382 games 2017 Jan >2000 Elo
 NEO4J_DB_PATH = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASS = "pass"
